81a2733a1c4e3ba5fc908ce213f73fcc7ca4b4e4
laszlo.dian <laszlo.dian@kuka.com>
2020-02-19 15:35:27 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SWB_OptionPackageInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPathOfOptionpackages = new System.Windows.Forms.TextBox();
+            this.tbPathOfSWB = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tbPathOfSWB, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbPathOfOptionpackages, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btStart, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path of optionpackages:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(3, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path of SWB:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbPathOfOptionpackages
+            // 
+            this.tbPathOfOptionpackages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.tbPathOfOptionpackages.Location = new System.Drawing.Point(403, 3);
+            this.tbPathOfOptionpackages.Name = "tbPathOfOptionpackages";
+            this.tbPathOfOptionpackages.Size = new System.Drawing.Size(394, 20);
+            this.tbPathOfOptionpackages.TabIndex = 2;
+            this.tbPathOfOptionpackages.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPathOfOptionpackages_KeyDown);
+            // 
+            // tbPathOfSWB
+            // 
+            this.tbPathOfSWB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPathOfSWB.Location = new System.Drawing.Point(403, 47);
+            this.tbPathOfSWB.Name = "tbPathOfSWB";
+            this.tbPathOfSWB.Size = new System.Drawing.Size(394, 20);
+            this.tbPathOfSWB.TabIndex = 3;
+            this.tbPathOfSWB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPathOfSWB_KeyDown);
+            // 
+            // btStart
+            // 
+            this.btStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btStart.Location = new System.Drawing.Point(646, 92);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(151, 35);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Start Installing";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tbPathOfSWB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPathOfOptionpackages;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWB_OptionPackageInstaller
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        private string pathOfSWB;
+
+        public string PathOfSWB
+        {
+            get { return pathOfSWB; }
+            set { pathOfSWB = value; }
+        }
+
+        private string pathOfOptionPackages;
+
+        public string PathOfOptionPackages
+        {
+            get { return pathOfOptionPackages; }
+            set { pathOfOptionPackages = value; }
+        }
+        #endregion
+
+
+        #region Variables
+        public DirectoryInfo tempDir; 
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbPathOfSWB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                pathOfSWB = tbPathOfSWB.Text.Trim();
+                CheckSWBPath();
+            }
+
+        }
+
+        private void tbPathOfOptionpackages_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                pathOfOptionPackages= tbPathOfOptionpackages.Text.Trim();
+                CheckOptionPackagePath();
+            }
+        }
+        public bool CheckSWBPath()
+        {
+            if (!Directory.Exists(PathOfSWB))
+            {
+                MessageBox.Show("The given path of the SWB does not exists!");
+
+                return false;
+            }
+            else
+                return true;
+        }
+        public bool CheckOptionPackagePath()
+        {
+            if (!Directory.Exists(PathOfOptionPackages))
+            {
+                MessageBox.Show("The given path of the optionpackages does not exists!");
+
+                return false;
+            }
+            else
+                return true;
+        }
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (!CheckOptionPackagePath())
+            {
+                CheckSWBPath();
+            }
+            PathOfOptionPackages = tbPathOfOptionpackages.Text.Trim();
+            PathOfSWB = tbPathOfSWB.Text.Trim();
+            InstallingProcess();
+            
+        }
+
+        private void InstallingProcess()
+        {
+            foreach (FileInfo optionPackage in new DirectoryInfo(PathOfOptionPackages).GetFiles("*.zip"))
+            {
+                String ZipPath = optionPackage.FullName;
+                tempDir = Directory.CreateDirectory(string.Format("{0}{1}temp", PathOfSWB, System.IO.Path.DirectorySeparatorChar));
+                String extractPath = tempDir.FullName;
+
+                ZipFile.ExtractToDirectory(ZipPath, extractPath);
+
+                DirectoryInfo destinationDirectory =
+                    Directory.CreateDirectory(string.Format("{0}{1}{2}{1}{3}", PathOfSWB, System.IO.Path.DirectorySeparatorChar, "features", optionPackage.Name));
+
+                ZipFile.ExtractToDirectory(string.Format(extractPath, Properties.Settings.Default.ContentZipFileName), destinationDirectory.FullName);
+
+                /*process of installation
+                 * unzip each zip file and copy the folders(plugins,features) content to the same named folder under the swb directory
+                 * create a new directory to the features folder (with the same name as the optionpackage
+                 * unzip content.jar file content to this newly created folder
+                 * start swb    (optional)
+                 */
+            }
+            Directory.Delete(tempDir.FullName, true);
+
+            MessageBox.Show("Installation of optionpackages is successfull! \r\n Start SWB then check the installed packages!");
+        }
+    }
+}
